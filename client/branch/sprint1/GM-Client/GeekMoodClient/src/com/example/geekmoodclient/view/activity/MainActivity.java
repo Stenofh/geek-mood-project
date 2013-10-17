ad2a3041d1261c066f9d67e859e47769eba00566
mandicaBrito <amanda.brito.191@gmail.com>
2013-10-16 21:51:49 -0400

    Adding layout files

--- client/branch/sprint1/GM-Client/GeekMoodClient/src/com/example/geekmoodclient/view/activity/MainActivity.java
+++ client/branch/sprint1/GM-Client/GeekMoodClient/src/com/example/geekmoodclient/view/activity/MainActivity.java
@@ -1,17 +1,47 @@
 package com.example.geekmoodclient.view.activity;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ListView;
 
 import com.example.geekmoodclient.R;
+import com.example.geekmoodclient.model.Mood;
+import com.example.geekmoodclient.presenter.IMainPresenter;
+import com.example.geekmoodclient.presenter.impl.MainPresenter;
+import com.example.geekmoodclient.view.IMainView;
+import com.example.geekmoodclient.view.adapter.MoodFeedAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener,
+	IMainView {
+
+    private MoodFeedAdapter adapter;
+    private ListView moodsFeedList;
+    private List<Mood> moods;
+    private IMainPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_main);
+
+	moodsFeedList = (ListView) findViewById(R.id.lst_moods_feed);
+
+	presenter = new MainPresenter(this);
+	moods = presenter.getMoodFeed();
+
+	adapter = new MoodFeedAdapter(moods, getContext(), this);
+	moodsFeedList.setAdapter(adapter);
+    }
+
+    @Override
+    public Context getContext() {
+	return this;
     }
 
     @Override
@@ -19,6 +49,12 @@ public class MainActivity extends Activity {
 	// Inflate the menu; this adds items to the action bar if it is present.
 	getMenuInflater().inflate(R.menu.main, menu);
 	return true;
+    }
+
+    @Override
+    public void onClick(View arg0) {
+	// TODO Auto-generated method stub
+
     }
 
 }
--- client/branch/sprint1/GM-Client/GeekMoodClient/src/com/example/geekmoodclient/view/activity/SplashActivity.java
+++ client/branch/sprint1/GM-Client/GeekMoodClient/src/com/example/geekmoodclient/view/activity/SplashActivity.java
@@ -2,21 +2,65 @@ package com.example.geekmoodclient.view.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.widget.ProgressBar;
 
 import com.example.geekmoodclient.R;
 
-public class SplashActivity extends Activity{
-    
+public class SplashActivity extends Activity {
+
+    private ProgressBar progressBar;
+    private ProgressTask task = new ProgressTask();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.splash);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        
-        finish();
+	super.onCreate(savedInstanceState);
+
+	setContentView(R.layout.splash);
+
+	progressBar = (ProgressBar) findViewById(R.id.progressBar);
+	task.execute(0);
+    }
+
+    private class ProgressTask extends AsyncTask<Integer, Integer, Void> {
+
+	protected void onPreExecute() {
+	    super.onPreExecute();
+	    progressBar.setMax(100);
+
+	}
+
+	protected void onCancelled() {
+	    progressBar.setMax(0);
+	    Log.v("Progress", "Cancelled");
+	}
+
+	protected Void doInBackground(Integer... params) {
+	    int start = params[0];
+	    for (int i = start; i <= 100; i += 20) {
+		try {
+		    progressBar.setProgress(i);
+		    SystemClock.sleep(500);
+		} catch (Exception e) {
+		    Log.e("Error", e.toString());
+		}
+	    }
+	    return null;
+	}
+
+	protected void onProgressUpdate(Integer... values) {
+	    progressBar.setProgress(5);
+	}
+
+	protected void onPostExecute(Void result) {
+	    startActivity(new Intent(SplashActivity.this,
+		    AuthenticationActivity.class));
+	    finish();
+	}
+
     }
 
 }
